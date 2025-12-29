04c5deded02ba2e554256f4c3c287c49028bec69
AnufrievSY <russianeagleowl@gmail.com>
2025-12-29 22:19:02 +0500

    ref: Обернул список с ссылками в тег nav по семантике, так как это навигация по сайту.

--- src/scripts/generate-pages.js
+++ src/scripts/generate-pages.js
@@ -26,6 +26,7 @@ const index = `<!doctype html>
 <html lang="ru">
 <head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Pages</title></head>
 <body>
+<nav>
   <ul>
     <li><a href="/login.html">login</a></li>
     <li><a href="/register.html">register</a></li>
@@ -34,6 +35,7 @@ const index = `<!doctype html>
     <li><a href="/404.html">404</a></li>
     <li><a href="/500.html">500</a></li>
   </ul>
+</nav>
 </body>
 </html>`;
 fs.writeFileSync(path.join(outDir, "index.html"), index, "utf8");
